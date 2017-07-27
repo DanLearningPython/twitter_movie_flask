d51e53ba2e94c209e8bcc403298616968d2838cc
Dan Mo <dmo@princeton.edu>
2017-07-27 15:38:32 -0400

    basic api - returns all tweets

--- twitter_movie_flask.py
+++ twitter_movie_flask.py
@@ -1,18 +1,44 @@
-from flask import Flask, render_template
-from config import *
+from flask import Flask, Response
+
+import json
 from flask_pymongo import PyMongo
-import sys
+
 app = Flask(__name__)
 app.config['MONGO_DBNAME'] = 'twitter'
 mongo = PyMongo(app)
 
+
 @app.route('/')
 def hello_world():
     tweets = mongo.db.movies.find({})
-    print(tweets);
-    for tweet in tweets:
-        print(tweet, file=sys.stderr)
-    return render_template('index.html',tweets=tweets)
+
+    json_result = to_json(tweets)
+
+    resp = Response(response=json_result,
+                    status=200,
+                    mimetype="application/json")
+
+    return resp
+
+
+def to_json(mongo_result):
+
+    json_result = []
+
+    for result in mongo_result:
+        tmp = {
+            'checksum': result['checksum'],
+            'topic': result['topic'],
+            'timestamp' : result['timestamp'],
+            'tweet': result['tweet'],
+            'sentiment': {
+                'positive': result['sentiment'][1],
+                'negative': result['sentiment'][0]
+            }
+        }
+        json_result.append(tmp.copy())
+
+    return json.dumps(json_result)
 
 
 if __name__ == '__main__':
